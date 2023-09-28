98c10774d4bef75caeca1069425c6070cc3bc8ad
abcrahulabc <141929043+abcrahulabc@users.noreply.github.com>
2023-09-28 17:26:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/L2LUCKY/SMART_QUEEN",
+    "https://github.com/abcrahulabc/QueenXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
